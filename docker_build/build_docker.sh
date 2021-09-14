afbb6d9ce39fa98f79d2e52b81940bd76fe87925
Amro Tork <amro_tork@mabrains.com>
2021-09-14 11:23:59 +0200

    - Adding docker push command.

--- docker_build/build_docker.sh
+++ docker_build/build_docker.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -f
 
-docker build ./
+#Analog Design Environment tag
+export OPEN_ANALOG_DESIGN_TAG="v0.1"
+export OPEN_ANALOG_DESIGN_NAME="mabrains/open-analog-design"
+
+
+docker build -t $OPEN_ANALOG_DESIGN_NAME:$OPEN_ANALOG_DESIGN_TAG
+docker push $OPEN_ANALOG_DESIGN_NAME:$OPEN_ANALOG_DESIGN_TAG
+
