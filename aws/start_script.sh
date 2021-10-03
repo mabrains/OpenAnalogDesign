d7ee5932e7f594e172a675a1601946418e9bf2ba
atorkmabrains <74936860+atorkmabrains@users.noreply.github.com>
2021-10-03 12:52:01 +0200

    Create start_script.sh

---
+++ aws/start_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -f
+
+sudo apt-get update
+sudo apt-get install git
+git clone https://github.com/mabrains/OpenAnalogDesign.git
+cd OpenAnalogDesign/docker_build/
+make all
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y dbus-user-session docker-ce-rootless-extras
